04da5a86944f67bb0d11a2a017d17002ca6bfdc9
Danny <danny@shoppimon.com>
2019-04-26 22:23:14 +0300

    use ui router with content

--- index.js
+++ index.js
@@ -7,24 +7,37 @@ export function bootstrap() {
 }
 
 export function mount() {
-    const promise = new Promise((resolve) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', '/page1.html');
-        xhr.send();
+  // Create the router
+  const router = new UIRouter();
+  window.router = router;
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                resolve(xhr.responseText);
-            }
-        };
-    });
+  router.plugin(Visualizer);
+  router.plugin(pushStateLocationPlugin);
+  router.plugin(servicesPlugin);
 
-    return promise.then(content =>
-        document.getElementById('content').innerHTML = 'TEST 2'
-    );
+  // This transition hook renders each active states' html property
+  router.transitionService.onSuccess({}, (trans, state) => {
+    trans.exiting().forEach(stateDef => naiveClearRenderedDom(stateDef));
+    trans.entering().forEach(stateDef => naiveRenderIntoDom(stateDef));
+  });
+
+  // Register some sample states
+  router.stateRegistry.register({ name: 'intro', url: '/bike-app', html:  'INTRO <a href="javascript:void(0)" onclick="router.stateService.go(\'page1\')">page 1</a>' });
+  router.stateRegistry.register({ name: 'page1', url: '/bike-app/page1', html: 'PAGE 1 <a href="javascript:void(0)" onclick="router.stateService.go(\'intro\')">back to intro</a>' });
+
+  // Start the router
+  router.trace.enable(1);
+  router.urlService.rules.initial({ state: 'intro' });
+  router.urlService.listen();
+  router.urlService.sync();
+
+  return Promise.resolve().then(() => document.getElementById('content').innerHTML = '<ui-view id="root"></ui-view>');
 }
 
 export function unmount() {
     return Promise.resolve()
-        .then(() => document.getElementById('content').innerHTML = '');
+        .then(() => {
+          delete window.router;
+          document.getElementById('content').innerHTML = '';
+        });
 }
